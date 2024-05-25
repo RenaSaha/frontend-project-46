00e9eb8181970f916890ed1729701b86bf4e8b56
root <root@LAPTOP-UOEK6B27>
2024-05-25 14:16:17 +0300

    added arguments and options

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -6,8 +6,10 @@ import { Command } from 'commander';
 
 const program = new Command();
 program
+    .version('0.1.0')
     .description('Compares two configuration files and shows a difference.')
-    .version('0.1.0');
+    .option('-f, --format <type>', 'output format', 'stylish')
+    .arguments('<filepath1> <filepath2>')
 
 
 program.parse(); 
